9f002d46ef75f3a4340e4e2c9d60c557b3e11eb6
balbesina228 <dolphie.guy@gmail.com>
2023-06-23 22:03:22 +0700

    upd urls.py to right

--- notepad/urls.py
+++ notepad/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('notes', views.all_notes, name='notes'),
     path('', views.greeting, name='home'),
     path('notes/create_post', views.create_post, name='create_post'),
-    path('notes/<id=int>', views.note, name='note')
+    path('notes/<int:id>', views.note, name='note')
 ]
